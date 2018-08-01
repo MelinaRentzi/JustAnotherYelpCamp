b99dee59bbddb6ab66657b7c50a2eaf5f7198801
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-01 08:26:05 +0000

    Added navbar with Bootstrap3 and centered the form page

--- app.js
+++ app.js
@@ -2,38 +2,39 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 var campgrounds = [
-        {name: "Salo", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg"},
-        {name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg"},
-        {name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg"}
-        ];
+    { name: "Salo", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
+    { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
+    { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
+    { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" }
+]
 
-app.get("/", function(req, res){
-   res.render("landing"); 
+app.get("/", function(req, res) {
+    res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-        res.render("campgrounds", {campgrounds: campgrounds});
+app.get("/campgrounds", function(req, res) {
+    res.render("campgrounds", { campgrounds: campgrounds });
 });
 
-app.get("/campgrounds/new", function(req, res){
-   res.render("new.ejs"); 
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs");
 });
 
-app.post("/campgrounds", function(req, res){
-   // get data from form and add them to campgrounds array
-   var name = req.body.name;
-   var image = req.body.image;
-   var newCampground = {name: name, image: image};
-   campgrounds.push(newCampground);
-   //redirect back to campgrounds page (default = redirect as a get request)
-   res.redirect("/campgrounds");
+app.post("/campgrounds", function(req, res) {
+    // get data from form and add them to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = { name: name, image: image };
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page (default = redirect as a get request)
+    res.redirect("/campgrounds");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server has started!"); 
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The YelpCamp Server has started!");
 });
