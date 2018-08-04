2760e52c617220f73d84f6f780be5e4bb0de25b9
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-04 15:23:53 +0000

    travis

--- app.js
+++ app.js
@@ -1,39 +1,95 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    { name: "Salo", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
-    { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
-    { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
-    { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" }
-]
+// Schema setup
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({ name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg", description: "This is a huge tent" }, function(err, campground) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log("NEWLY CREATED CAMPGROUND: ");
+//         console.log(campground);
+//     }
+// });
+
+// var campgrounds = [
+//     { name: "Salo", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
+//     { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
+//     { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" },
+//     { name: "Mounts", image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Camp_4.jpg" }
+// ]
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// INDEX - Show all campgrounds
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", { campgrounds: campgrounds });
+    // get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("index", { campgrounds: allCampgrounds });
+        }
+    });
 });
 
+// NEW - Show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
     res.render("new.ejs");
 });
 
+//CREATE - Add new campground to DB
 app.post("/campgrounds", function(req, res) {
     // get data from form and add them to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = { name: name, image: image };
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds page (default = redirect as a get request)
-    res.redirect("/campgrounds");
+    var desc = req.body.description;
+    var newCampground = { name: name, image: image, description: desc };
+    //Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            //redirect back to campgrounds page (default = redirect as a get request)
+            res.redirect("/campgrounds");
+        }
+    });
+
 });
+
+// SHOW - Shows more info about 1 campground
+app.get("/campgrounds/:id", function(req, res) {
+    // find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // render show template with that campground
+            res.render("show", { campground: foundCampground });
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp Server has started!");
