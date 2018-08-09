c1d256bc018da9269ee7dad9b20f41df0d27a73c
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-09 16:18:02 +0000

    authentication for register and login added

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
+    User = require("./models/user"),
     seedDB = require("./seeds")
 
 mongoose.connect("mongodb://localhost/yelp_camp");
@@ -12,6 +15,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Besamel",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -111,6 +126,42 @@ app.post("/campgrounds/:id/comments", function(req, res) {
     //redirect to campground show page
 });
 
+// =====================//
+// AUTHORIZATION ROUTES //
+// =====================//
+
+// show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+// handle signup logic
+app.post("/register", function(req, res) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+// handle login logic
+// middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+
+}), function(req, res) {
+
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp Server has started!");
