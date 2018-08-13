f8d36fc1d606e80a3d5778e5c7ea7f7bf18b9dca
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-13 07:33:05 +0000

    Added Edit Campgrounds functionality and method-override

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Campground = require("../models/campground");
 
 // INDEX - Show all campgrounds
-router.get("/", isLoggedIn, function(req, res) {
+router.get("/", function(req, res) {
     // get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds) {
         if (err) {
@@ -58,6 +58,33 @@ router.get("/:id", function(req, res) {
         }
     });
 });
+
+// EDIT Campground ROUTE
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            res.redirect("/campgrounds");
+        }
+        else {
+            res.render("campgrounds/edit", { campground: foundCampground });
+        }
+    });
+});
+
+router.put("/:id", function(req, res) {
+    // find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+        if (err) {
+            res.redirect("/campgrounds");
+        }
+        else {
+            //redirect to the show page
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// UPDATE Campground ROUTE
 
 //middleware
 function isLoggedIn(req, res, next) {
