0ee1ee5ccf931d1cf025b8f6f20eff60f8c1db94
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-13 07:45:27 +0000

    Added DELETE functionality and button

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -71,6 +71,7 @@ router.get("/:id/edit", function(req, res) {
     });
 });
 
+// UPDATE Campground ROUTE
 router.put("/:id", function(req, res) {
     // find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
@@ -84,7 +85,17 @@ router.put("/:id", function(req, res) {
     });
 });
 
-// UPDATE Campground ROUTE
+// DESTROY Campground ROUTE
+router.delete("/:id", function(req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        }
+        else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
 
 //middleware
 function isLoggedIn(req, res, next) {
